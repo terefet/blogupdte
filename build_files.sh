4a18c09631528ab069dcec5047a2046cd6b9351e
terefet <tutg24@gmail.com>
2024-01-29 13:08:26 +0300

    readd config file

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
-pip install -r requirements.txt
+echo "Building project packages.."
+
+python3.9 -m pip install -r requirements.txt
+
+echo "Migrating Database..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "collecting static files..."
 python3.9 manage.py collectstatic --noinput
