c80a955c54c222ffba0073424120ce5c09209cdb
terefet <tutg24@gmail.com>
2024-01-29 14:15:12 +0300

    bug fix2

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,6 +89,8 @@ DATABASES = {
         "PORT": os.environ.get("DB_PORT"),
     }
 }
+
+DATABASES['default'] = dj_database_url.config()
 
 
 
